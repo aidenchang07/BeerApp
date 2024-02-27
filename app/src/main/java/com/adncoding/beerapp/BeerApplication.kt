e2df4306b1d44950bf281e5796296655b6a0e32e
Aiden Chang <aidenchang@pcpartner.com>
2024-02-27 19:58:56 +0800

    Add BeerApplication

---
+++ app/src/main/java/com/adncoding/beerapp/BeerApplication.kt
@@ -0,0 +1,10 @@
+package com.adncoding.beerapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by AidenChang 2024/02/27
+ */
+@HiltAndroidApp
+class BeerApplication: Application()
